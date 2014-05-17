d7bce599a06ae6c2ecc97fe7bc05a8d8118cb8de
ssheremet <sergey.sheremet@gmail.com>
2014-05-17 23:43:58 +0300

    My version 2
    readDataSet function changed

--- readDataSet.R
+++ readDataSet.R
@@ -5,5 +5,7 @@ readDataSet <- function(directory, dateFrom, dateTo){
        dataFile <- read.csv(directory, sep = ";", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings = "?")
        
        ##filter 
-       readDataSet <- suppressWarnings(subset(dataFile, as.Date(as.character(dataFile$Date, "%d/%m/%Y"), "%d/%m/%Y") == as.Date(c(dateFrom, dateTo))))
+       dataFile$Date <- as.Date(dataFile$Date, format='%d/%m/%Y')
+       readDataSet <- dataFile[dataFile$Date %in% as.Date(c(dateFrom, dateTo)),]
+       
 }
